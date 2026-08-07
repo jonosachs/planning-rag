7d3dfaab62349513494e9899c7c5931655fc7e62
Jono Sachs <jonosachs@gmail.com>
2026-08-07 14:09:27 +1000

    refactor(chromadb): Use reproducible db ids, make n_results global variable, commenting

--- src/indexing/chromadb.py
+++ src/indexing/chromadb.py
@@ -1,7 +1,9 @@
+from typing import Any
 from src.indexing.interfaces import VectorStore
 from src.indexing.schemas import EmbeddedChunk
 import chromadb
-import uuid
+
+N_RESULTS = 10
 
 
 class ChromaDb(VectorStore):
@@ -19,9 +21,12 @@ class ChromaDb(VectorStore):
     def write(self, embedded_chunks: list[EmbeddedChunk]):
         collection = self.client.get_or_create_collection(name=self.collection)
         for chunk in embedded_chunks:
+            # Create unique, reproducible id to avoid duplication
+            ids = f"{chunk.metadata['scheme_id']}:{chunk.metadata['ordinance_id']}:{chunk.metadata['chunk_index']}"
             try:
+                # 'upsert' overwrites an entry if it exists
                 collection.upsert(
-                    ids=[str(uuid.uuid4())],
+                    ids=[ids],
                     documents=[chunk.text],
                     embeddings=[chunk.embedded_text],
                     metadatas=[chunk.metadata],
@@ -29,8 +34,14 @@ class ChromaDb(VectorStore):
             except ValueError as e:
                 raise RuntimeError(f"⚠️ Failed to write to db: {e}") from e
 
-    # Query by vector similarity
-    def run_query(self, embedded_query: list[float], n_results: int = 10) -> dict:
+    def run_query(
+        self, embedded_query: list[float], n_results: int = N_RESULTS
+    ) -> dict[str, Any]:
+        """
+        Query by vector similarity
+        Response shape: https://docs.trychroma.com/reference/chroma-api/record/query-collection#response-metadatas-one-of-0
+        """
+
         collection = self.client.get_or_create_collection(name=self.collection)
         results = collection.query(
             query_embeddings=[embedded_query], n_results=n_results
@@ -38,6 +49,8 @@ class ChromaDb(VectorStore):
         return results
 
     def get_all(self) -> dict:
-        collection = self.client.get_or_create_collection(name=self.collection)
+        """Return all records from 'documents' and 'metadatas' fields"""
+
+        collection = self.client.get_collection(name=self.collection)
         records = collection.get(include=["documents", "metadatas"])
         return records
